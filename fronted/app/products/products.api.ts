461213d8c6dfe8826aeee4074d4338d23d67ef9e
Rodolfo Castro <soyracm@gmail.com>
2026-08-04 15:22:43 -0400

    variables

--- fronted/app/products/products.api.ts
+++ fronted/app/products/products.api.ts
@@ -1,15 +1,17 @@
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+
 export async function getProducts() {
-  const data = await fetch('http://localhost:4000/api/products');
+  const data = await fetch(`${BACKEND_URL}/api/products`);
   return await data.json();
 }
 
 export async function getProduct(id: string) {
-  const data = await fetch(`http://localhost:4000/api/products/${id}`,{});
+  const data = await fetch(`${BACKEND_URL}/api/products/${id}`,{});
   return await data.json();
 }
 
 export async function createProduct(product: { name: string, description: string, price: number, images: string }) {
-  return await fetch('http://localhost:4000/api/products', {
+  return await fetch(`${BACKEND_URL}/api/products`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -19,14 +21,14 @@ export async function createProduct(product: { name: string, description: string
 }
 
 export async function deleteProduct(id: string) {
-  return await fetch(`http://localhost:4000/api/products/${id}`, {
+  return await fetch(`${BACKEND_URL}/api/products/${id}`, {
     method: 'DELETE',
   })
 }
 
 // Separamos el 'id' del resto de los datos (name, description, price, images)
 export async function updateProduct(id: string, product: { name: string, description: string, price: number, images: string }) {
-  return await fetch(`http://localhost:4000/api/products/${id}`, {
+  return await fetch(`${BACKEND_URL}/api/products/${id}`, {
     method: 'PATCH', // Debe ser PATCH para coincidir con NestJS
     headers: {
       'Content-Type': 'application/json',
